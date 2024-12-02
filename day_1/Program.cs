b693489b3b5b3c9f0c5a7f6b0d677420ba5e9159
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2024-12-02 11:11:11 -0800

    lines are split and cast to int, Quicksort implimentation finished and arrays sorted for final count.

--- day_1/Program.cs
+++ day_1/Program.cs
@@ -7,11 +7,20 @@ namespace AOCDay1
     {
         public static void Main(string[] args)
         {
+            List<int> left_ids = [];
+            List<int> right_ids = [];
             List<string> lines = Parse_Input("input.txt");
             foreach (var line in lines)
             {
-                Console.WriteLine(line);
+                var ids = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                left_ids.Add(int.Parse(ids[0]));
+                right_ids.Add(int.Parse(ids[1]));
             }
+
+            Quicksort(left_ids, 0, left_ids.Count - 1);
+            Quicksort(right_ids, 0, right_ids.Count - 1);
+
+
         }
 
         static List<string> Parse_Input(string path)
@@ -24,6 +33,46 @@ namespace AOCDay1
                 result.Add(line);
             }
             return result;
+        }
+
+        static int Partition(List<int> arr, int left, int right)
+        {
+            int pivot = arr[left];
+            int i = left + 1;
+            int j = right;
+
+            while (true)
+            {
+                while (i <= j && arr[i] <= pivot) i++;
+                while (i <= j && arr[j] > pivot) j--;
+
+                if (i <= j)
+                {
+                    SwapArrayValues(arr, i, j);
+                    i++;
+                    j--;
+                }
+                else
+                {
+                    SwapArrayValues(arr, left, j);
+                    return j;
+                }
+            }
+        }
+
+        static void Quicksort(List<int> arr, int left, int right)
+        {
+            if (left >= right) return;
+            int pivot = Partition(arr, left, right);
+            Quicksort(arr, left, pivot - 1);
+            Quicksort(arr, pivot + 1, right);
+        }
+
+        static void SwapArrayValues(List<int> arr, int i, int j)
+        {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
 }
