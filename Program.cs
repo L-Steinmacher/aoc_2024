96775041edd7acf8fd0467caf9b4808c9244d222
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2024-12-01 17:31:42 -0800

    reading all lines and printing them off

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace AOCDay1
+{
+    public class Project
+    {
+        public static void Main(string[] args)
+        {
+            List<string> lines = Parse_Input("input.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static List<string> Parse_Input(string path)
+        {
+            List<string> result = [];
+            var file_contents = File.ReadAllLines(path);
+            for (int i = 0; i < file_contents.Length; i++)
+            {
+                string line = file_contents[i];
+                result.Add(line);
+            }
+            return result;
+        }
+    }
+}
