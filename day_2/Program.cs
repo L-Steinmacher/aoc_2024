0ea8af1203b34386290c833b4b38ad226a1f7090
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2024-12-03 11:54:26 -0800

    part 1 passing

--- day_2/Program.cs
+++ day_2/Program.cs
@@ -11,15 +11,45 @@ namespace AOCDay2
 
         static void Main(string[] args)
         {
+            int safeReports = 0;
             List<List<int>> reports = new();
             List<string> lines = Utilities.Parse_Input("input.txt");
             foreach (var line in lines)
             {
                 string[] reportString = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
                 List<int> report = new(Array.ConvertAll(reportString, int.Parse));
+                bool safety = AssessReportSafety(report);
+                if (safety) safeReports += 1;
+            }
+            Console.WriteLine(safeReports);
+
+        }
+
+        public static bool AssessReportSafety(List<int> report)
+        {
+            bool safe = true;
+            int initialTrend = report[0] - report[1];
+            int safeStep = 3;
+            for (int i = 1; i < report.Count; i++)
+            {
+
+                int currTrend = report[i - 1] - report[i];
+                // Console.WriteLine($"currDiff: {currDiff}");
+
+                if (Math.Sign(currTrend) != Math.Sign(initialTrend))
+                {
+                    safe = false;
+                    break;
+                }
+
+                if (Math.Abs(currTrend) > safeStep)
+                {
+                    safe = false;
+                    break;
+                }
             }
 
-            Console.WriteLine(reports);
+            return safe;
         }
     }
 }
