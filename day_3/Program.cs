e5a9065818369231e23f32a9c7bd7260e519a2f1
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2024-12-04 10:21:59 -0800

    finding all the muls

--- day_3/Program.cs
+++ day_3/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Metrics;
 using System.IO;
+using System.Text.RegularExpressions;
 using AOC2024.Common;
 
 namespace AOCDay3
@@ -12,7 +13,22 @@ namespace AOCDay3
             List<string> lines = Utilities.Parse_Input("input.txt");
             foreach (var line in lines)
             {
-                Console.WriteLine(line);
+                // Console.WriteLine(line);
+                MulFinder(line);
+            }
+        }
+
+        private static void MulFinder(string line)
+        {
+            int accumulator = 0;
+            var pairs = new List<(int, int)>();
+            string pattern = @"mul\((\d+),\s*(\d+)\)";
+
+            foreach (Match match in Regex.Matches(line, pattern))
+            {
+                Console.WriteLine("'{0}' found at index {1}.", match.Value, match.Index);
+                // accumulator += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+                // Console.WriteLine(accumulator);
             }
         }
     }
