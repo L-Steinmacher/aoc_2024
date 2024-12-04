4a2189f52c23891a728bad4464b76e65dd2d99c7
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2024-12-03 19:06:40 -0800

    day_3 first commit

---
+++ day_3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.IO;
+using AOC2024.Common;
+
+namespace AOCDay3
+{
+    class MemoryRecovery
+    {
+        static void Main(string[] args)
+        {
+            List<string> lines = Utilities.Parse_Input("input.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
