3e248a34b3c50f67a124304bfe501d4271ba00e3
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2024-12-05 19:51:20 -0800

    Extension method for strings written and braught into day_4

--- day_4/Program.cs
+++ day_4/Program.cs
@@ -2,19 +2,34 @@
 using System.Diagnostics.Metrics;
 using System.IO;
 using AOC2024.Common;
-
+using ExtensionMethods;
 
 namespace AOCDay4
 {
+
     public class WordSearch
     {
         static void Main(string[] args)
         {
             List<string> lines = Utilities.Parse_Input("input.txt");
+            List<List<string>> letterArray = lines.Select(line => line.Split("").ToList()).ToList();
             foreach (var line in lines)
             {
-                Console.WriteLine(line);
+                var result = line.AllIndexesOf("X");
+                foreach (var r in result)
+                {
+                    Console.WriteLine(r);
+                }
             }
+            for (int i = 0; i < letterArray.Count; i++)
+            {
+
+            }
+        }
+
+        public int FindXMAS()
+        {
+            return 0;
         }
     }
 }
