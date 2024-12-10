03ed1f2d96e5480de40444022bdb1475fc6b4353
Lucas Steinmacher <lucaslsteinmacher@gmail.com>
2024-12-10 14:17:36 -0800

    day_5 first commit.  reading lines and splitting into rules and UpdatePages

---
+++ day_5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AOC2024.Common;
+using Microsoft.VisualBasic;
+
+namespace AOCDay5
+{
+    public class Queue
+    {
+        public static Dictionary<int, List<int>> OrderRules = new();
+        public static List<List<int>> UpdatePages = new();
+        static void Main(string[] args)
+        {
+            var lines = Utilities.Parse_Input("input.txt");
+            foreach (var line in lines)
+            {
+                if (line == "")
+                {
+                    continue;
+                }
+                else if (line.Length <= 5)
+                {
+                    var digits = line.Split('|');
+                    int before = int.Parse(digits[0]);
+                    int after = int.Parse(digits[1]);
+                    if (!OrderRules.ContainsKey(before))
+                    {
+                        OrderRules[before] = new List<int>();
+                        OrderRules[before].Add(after);
+                    }
+                }
+                else
+                {
+                    UpdatePages.Add(line.Split(',').Select(int.Parse).ToList());
+                }
+
+                // Console.WriteLine(line);
+            }
+            foreach (var line in UpdatePages)
+            {
+                string outputString = line.Select(x => x.ToString()).Aggregate((a, b) => a + " " + b);
+                Console.WriteLine(outputString);
+            }
+        }
+    }
+}
